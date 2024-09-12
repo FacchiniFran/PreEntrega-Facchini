fec6675a0b4b1910f5566cd934e57be0df7f196c
FacchiniFran <facchinifn@gmail.com>
2024-09-12 03:43:00 -0300

    al día

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import NavBar from './components/NavBar.jsx'
+import ItemListContainer from './components/ItemListContainer.jsx'
 
 function App() {
 
   return (
     <>
       <NavBar />
+      <ItemListContainer greeting='Bienvenidos a Mana Gamers E-Shop'/>
     </>
   )
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,10 +1,10 @@
-import './ItemListContainer.css'
-
-function ItemListContainer({}) {
+function ItemListContainer({ greeting }) {
 
     return (
         <>
-            
+            <section className='w-auto p-3 bg-info position-absolute top-50 start-50 translate-middle rounded'>
+                <h1 className='text-center text-body-emphasis'>{greeting}</h1>
+            </section>
         </>
     )
 }
